c40cdd7d4d3e7561fc9eae9f77401d2984ac7502
chrism3027 <chris.m.mcclure@gmail.com>
2015-10-12 20:37:30 -0500

    make supercool scripts YO!

--- Javascript/myjavascript.js
+++ Javascript/myjavascript.js
@@ -1,1 +1,16 @@
-alert('Hello World');
+//alert('From External JS File');
+document.getElementById("date").innerHTML = Date();
+
+function myFunction() {
+
+var first = parseInt(document.getElementById("num1").value);
+var second = parseInt(document.getElementById("num2").value);
+var third = parseInt(document.getElementById("num3").value);
+var total = first * second * third;
+document.getElementById("result").innerHTML = total;
+return false;
+};
+
+function ReloadFunction() {
+    location.reload();
+};
