ea8ac80682ec50728fd798e2e7c7c1333c90a5a5
chrism3027 <chris.m.mcclure@gmail.com>
2015-10-15 18:35:23 -0500

    add sweet drinking function

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -14,7 +14,16 @@ return false;
 
 function multiplySelf(x) {
 var total = x * x;
-document.getElementById('totalsq').innerHTML = total;
+c
 return false;
 }
 
+function drinkingAge(y) {
+if(y >=21){
+var answer = "Drink up motherfucker!";
+} else{
+var answer ="You are not old enough drink, go fuck yourself!";
+}
+document.getElementById('ageanswer').innerHTML = answer;
+    return false;
+}
