f14e0a2040afa57f569f0d20ccb94279bdf46d26
chrism3027 <chris.m.mcclure@gmail.com>
2015-11-04 20:58:53 -0600

    make it better

--- Javascript/DatePicker.js
+++ Javascript/DatePicker.js
@@ -1,37 +1,43 @@
 var day = 24 * 60 * 60 * 1000;
 
-$(function() {
-    $( "#from" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 2,
-         changeYear: true,
-      onClose: function( selectedDate ) {
-        $( "#to" ).datepicker( "option", "minDate", selectedDate );
-      }
+$(function () {
+    $("#from").datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 2,
+        changeYear: true,
+        showButtonPanel: true,
+        onClose: function (selectedDate) {
+            $("#to").datepicker("option", "minDate", selectedDate);
+        }
     });
-    $( "#to" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 2,
-         changeYear: true,
-      onClose: function( selectedDate ) {
-        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
-      }
+    $("#to").datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 2,
+        changeYear: true,
+
+        showButtonPanel: true,
+        onClose: function (selectedDate) {
+            $("#from").datepicker("option", "maxDate", selectedDate);
+        }
     });
-  });
+});
 
 
+$('#datebutton').click(function () {
 
-
-
-$('#datebutton').click(function(){
     var startDate = $('#from').val();
     var startDateOb = new Date(startDate);
     var endDate = $('#to').val();
     var endDateOb = new Date(endDate);
-    var difference = Math.ceil((endDateOb - startDateOb)/day);
-    $('#startDate').text(startDate);
-    $('#endDate').text(endDate);
-    $('#daysBetween').text(difference);
+    if (startDate == "" || endDate == "") {
+       $('#notRight').text('Please enter 2 valid dates!');
+
+    } else {
+        var difference = Math.ceil((endDateOb - startDateOb) / day);
+        $('#startDate').text(startDate);
+        $('#endDate').text(endDate);
+        $('#daysBetween').text(difference);
+    }
 });
